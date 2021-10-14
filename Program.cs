0f44407bd8265c5a49a46acec38776f644f45446
Brennan Collins <BCOLLI7351@my.sullivan.edu>
2021-10-14 19:35:29 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSC240_03_02_PhoneCall_bpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
